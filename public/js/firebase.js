a9ad9812c37e9f145dca01536b6849e94cbea8ce
Ryan <ryan.hopper-lowe@sjsu.edu>
2019-03-08 21:39:44 -0800

    moved firebase logic into firebase.js

---
+++ public/js/firebase.js
@@ -0,0 +1,29 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCvT55qbatIH6Gx8Mn2Ns4Ujx3v2t7RdQA",
+  authDomain: "styrax-47f13.firebaseapp.com",
+  databaseURL: "https://styrax-47f13.firebaseio.com",
+  projectId: "styrax-47f13",
+  storageBucket: "styrax-47f13.appspot.com",
+  messagingSenderId: "524300593780"
+};
+firebase.initializeApp(config);
+
+document.addEventListener('DOMContentLoaded', function() {
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  //
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  try {
+    let app = firebase.app();
+    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+  } catch (e) {
+    console.error(e);
+  }
+});
